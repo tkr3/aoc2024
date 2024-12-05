9688a7ba014d33f5ffe410e285bf0e9a471c1634
tkr3 <tobias.kreissl@gmx.de>
2024-12-05 08:14:13 +0100

    day05 part1

---
+++ src/bin/day_05.rs
@@ -0,0 +1,92 @@
+use std::collections::HashSet;
+
+pub fn part1(input: &str) -> u32 {
+    let (r, updates) = input.split_once("\n\n").unwrap();
+    let rules: HashSet<(u32, u32)> = r
+        .lines()
+        .map(|line| {
+            let mut numbers = line.split('|').filter_map(|n| n.parse().ok());
+            (numbers.next().unwrap(), numbers.next().unwrap())
+        })
+        .collect();
+
+    let mut mid_elements = Vec::new();
+    'outer: for update in updates.lines() {
+        let mut elements = Vec::new();
+        for current in update.split(',').filter_map(|x| x.parse().ok()) {
+            for previous in &elements {
+                if rules.contains(&(current, *previous)) {
+                    continue 'outer;
+                }
+            }
+            elements.push(current);
+        }
+        mid_elements.push(elements[(elements.len() - 1) / 2])
+    }
+
+    mid_elements.into_iter().sum()
+}
+
+pub fn part2(input: &str) -> u32 {
+    0
+}
+
+const INPUT: &str = include_str!("../../inputs/day_05.txt");
+
+fn main() {
+    let p1 = part1(INPUT);
+    println!("Part 1: {p1}");
+
+    let p2 = part2(INPUT);
+    println!("Part 2: {p2}");
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const EXAMPLE_INPUT_1: &str = "\
+47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47
+";
+
+    #[test]
+    fn test_part_1() {
+        assert_eq!(part1(EXAMPLE_INPUT_1), 143);
+    }
+
+    const EXAMPLE_INPUT_2: &str = "\
+s
+";
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(part2(EXAMPLE_INPUT_2), 0);
+    }
+}
