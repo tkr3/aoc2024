88c5e88583e26f8761b083673806d1427a057ed1
tkr3 <tobias.kreissl@gmx.de>
2024-12-05 20:39:48 +0100

    day05 optimisation

---
+++ benches/bench.rs
@@ -0,0 +1,9 @@
+use criterion::{criterion_group, criterion_main};
+
+// To run individual benchmarks use:
+// $ cargo bench --bench bench -- <name>
+// where <name> can be like: day_07, 07, 07/1, 7/2
+
+aoc2024::benches!(day_01, day_02, day_03, day_04, day_05);
+
+criterion_main!(benchmarks);
--- src/bin/day_05.rs
+++ src/bin/day_05.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::{cmp::Ordering, collections::HashSet};
 
 pub fn part1(input: &str) -> u32 {
     let (r, updates) = input.split_once("\n\n").unwrap();
@@ -10,21 +10,17 @@ pub fn part1(input: &str) -> u32 {
         })
         .collect();
 
-    let mut mid_elements = Vec::new();
-    'outer: for update in updates.lines() {
-        let mut elements = Vec::new();
-        for current in update.split(',').filter_map(|x| x.parse().ok()) {
-            for previous in &elements {
-                if rules.contains(&(current, *previous)) {
-                    continue 'outer;
-                }
+    updates
+        .lines()
+        .map(|update| update.split(',').filter_map(|x| x.parse().ok()))
+        .filter_map(|update| {
+            let update: Vec<u32> = update.collect();
+            if update.is_sorted_by(|a, b| rules.contains(&(*a, *b))) {
+                return Some(update[update.len() / 2]);
             }
-            elements.push(current);
-        }
-        mid_elements.push(elements[(elements.len() - 1) / 2])
-    }
-
-    mid_elements.into_iter().sum()
+            None
+        })
+        .sum()
 }
 
 pub fn part2(input: &str) -> u32 {
@@ -37,51 +33,31 @@ pub fn part2(input: &str) -> u32 {
         })
         .collect();
 
-    let mut mid_elements = Vec::new();
-    for update in updates
+    updates
         .lines()
         .map(|update| update.split(',').filter_map(|x| x.parse().ok()))
-    {
-        let mut update: Vec<u32> = update.collect();
-        let mut modified = false;
-        'outer: loop {
-            // Find next wrong element
-            for (x, current) in update.iter().enumerate() {
-                for (y, previous) in update[..x].iter().enumerate() {
-                    if rules.contains(&(*current, *previous)) {
-                        // Found wrong pair, swap them
-                        update.swap(x, y);
-                        modified = true;
-                        continue 'outer;
-                    }
-                }
+        .filter_map(|update| {
+            let mut update: Vec<u32> = update.collect();
+            if update.is_sorted_by(|a, b| rules.contains(&(*a, *b))) {
+                return None;
             }
-            break;
-        }
-        // No more wrong elements
-        if !modified {
-            continue;
-        }
-        mid_elements.push(update[(update.len() - 1) / 2])
-    }
-    mid_elements.into_iter().sum()
+            update.sort_unstable_by(|a, b| {
+                if rules.contains(&(*a, *b)) {
+                    return Ordering::Less;
+                } else if rules.contains(&(*b, *a)) {
+                    return Ordering::Greater;
+                }
+                Ordering::Equal
+            });
+            Some(update[(update.len() - 1) / 2])
+        })
+        .sum()
 }
 
-const INPUT: &str = include_str!("../../inputs/day_05.txt");
+aoc2024::main!("../../inputs/day_05.txt");
 
-fn main() {
-    let p1 = part1(INPUT);
-    println!("Part 1: {p1}");
-
-    let p2 = part2(INPUT);
-    println!("Part 2: {p2}");
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    const EXAMPLE_INPUT_1: &str = "\
+aoc2024::test!(
+    "\
 47|53
 97|13
 97|61
@@ -110,17 +86,7 @@ mod test {
 75,97,47,61,53
 61,13,29
 97,13,75,29,47
-";
-
-    #[test]
-    fn test_part_1() {
-        assert_eq!(part1(EXAMPLE_INPUT_1), 143);
-    }
-
-    const EXAMPLE_INPUT_2: &str = EXAMPLE_INPUT_1;
-
-    #[test]
-    fn test_part_2() {
-        assert_eq!(part2(EXAMPLE_INPUT_2), 123);
-    }
-}
+",
+    143,
+    123
+);
